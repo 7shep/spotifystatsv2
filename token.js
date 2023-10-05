dff2bd8e63dd45a75b76daeab1c242a9d7af3359
Alex <68706402+7shep@users.noreply.github.com>
2023-10-05 11:36:10 -0400

    no work no mo

--- token.js
+++ token.js
@@ -1,63 +1,56 @@
-//Imports
 const express = require('express');
 const axios = require('axios');
 const querystring = require('querystring');
 
-//Spotify API
 const clientid = '23891eeee9ee42b8aaf68a066f0d525d';
 const clientsecret = '013fc4f179c849e389414f69dc73f619';
 const redirecturi = 'http://127.0.0.1:5550/loginsuccess.html';
 
 const app = express();
 
-//When the express server detects the user being redirected to loginsuccess.html
-app.get('/loginsuccess.html', (req, res) => {
-    // Check if the request contains an authorization code
+let accessToken = ''; // Variable to store the access token
+
+app.get('/loginsuccess.html', async (req, res) => {
     const code = req.query.code;
 
-    // Making sure the user has an Authorization code
     if (!code) {
         res.status(400).send('Authorization code not found');
         return;
     }
 
-    //Sends a 'POST' request to access the users access token and refresh token!!
-    axios({
-        method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
-        data: querystring.stringify({
+    try {
+        const response = await axios.post('https://accounts.spotify.com/api/token', querystring.stringify({
             grant_type: 'authorization_code',
             code: code,
             redirect_uri: redirecturi,
-        }),
-        headers: {
-            'Authorization': 'Basic ' + (new Buffer.from(clientid + ':' + clientsecret).toString('base64')),
+        }), {
+            headers: {
+                'Authorization': 'Basic ' + Buffer.from(clientid + ':' + clientsecret).toString('base64'),
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
 
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-    })
-    .then(response => {
-        // Access token and refresh token are in the response data
-        const accessToken = response.data.access_token;
+        accessToken = response.data.access_token; // Store the access token in the variable
+
         const refreshToken = response.data.refresh_token;
         const expires = response.data.expires_in;
-        
 
-        // Use the access token to make Spotify API requests or store it for future use
-        // You can also store the refresh token for refreshing the access token
         console.log('Access Token:', accessToken);
         console.log('Refresh Token:', refreshToken);
-        console.log('Expires In: ', expires);
+        console.log('Expires In:', expires);
 
         res.redirect('http://127.0.0.1:5550/loginsuccess.html');
-    })
-    .catch(error => {
+    } catch (error) {
         console.error('Error exchanging authorization code for access token:', error);
         res.status(500).send('Error exchanging authorization code for access token');
-    });
+    }
 });
 
-//Listens for any server pings on port 5550, allows line 12 to work
+// Add an endpoint to retrieve the access token
+app.get('/getAccessToken', (req, res) => {
+    res.json({ accessToken });
+});
+
 app.listen(5550, (host, port) => {
     console.log('ShepStatsv2 listening on port http://%s%s', host, port);
 });
